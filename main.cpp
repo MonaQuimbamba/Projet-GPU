a7260ba0547b1fa3694a59a462cd70a9e0b01aad
Salhi Faisal <faisal.salhi@etu.unilim.fr>
2021-11-21 18:48:38 +0100

    Écrire une ébauche de la fonction de recherche des nombres premiers.

--- main.cpp
+++ main.cpp
@@ -4,15 +4,17 @@
 #include <iostream> // cout, endl
 #include <stdint.h> // uint64_t
 #include <tgmath.h> // sqrt()
+#include <vector>   // tableaux dynamiques <vector>
 
 bool isPrimeCPU(const uint64_t N);
 
 int main() {
     std::cout << "Programme de test partie CPU - Faisal." << std::endl;
+
     return 0;
 }
 
-/*  \brief  Je suis la méthode qui renvoit si un certain nombre N
+/**  \brief  Je suis la méthode qui renvoit si un certain nombre N
  *          est premier.
  *  @param N le nombre dont on doit tester la primalité.
  */
@@ -26,5 +28,22 @@ bool isPrimeCPU(const uint64_t N)
     }
     return true;
 }
+
+/** \brief  Je suis la méthode qui renvoit un tableau de nombre premier
+ *          avec pour limite N.
+ *  @param  limite La borne supérieur de l'ensemble que l'on souhaite évaluer.
+ */
+std::vector<uint64_t> searchPrimesCPU(const uint64_t limite){
+    std::vector<uint64_t> resultat(0);
+
+    for (uint64_t possiblePrime = limite-1; possiblePrime >=2; possiblePrime-=1){
+        if (isPrimeCPU(possiblePrime)) {
+            resultat.push_back(possiblePrime);
+        }
+    }
+    return resultat;
+}
+
+
 
 
