7535f4258949c5198e3a485bb0a71908c7616845
Salhi Faisal <faisal.salhi@etu.unilim.fr>
2021-11-30 14:13:49 +0100

    Créer les fichiers contenant les tests de performances.

---
+++ benchmark.cpp
@@ -0,0 +1,38 @@
+#include "benchmark.h"
+
+/** \brief  Je suis une fonction qui fait des mesures de temps pour analyser les performances
+ *          des algorithmes de calcul de la primalité d'un nombre.
+ *          (temps/s)
+ *          ^
+ *          |
+ *          |
+ *          |______________> (log2(N)) avec N choisit pour créer une échelle logarithmique
+ *
+ */
+void drawPlotForPrimalityTestAlgorithms(){
+
+}
+
+/** \brief  Je suis une fonction qui fait des mesures de temps pour analyser les performances
+ *          des algorithmes de recherches de nombres premiers jusqu'une certaine borne N.
+ *          (temps/s)
+ *          ^
+ *          |
+ *          |
+ *          |________________> N(Bornes Supérieures)
+ */
+void drawPlotForResearchOfPrimes(){
+
+}
+
+/** \brief  Je suis une fonction qui fait des mesures de temps pour analyser les performances
+ *          des algorithmes de factorisation en nombres premiers.
+ *          (temps/s)
+ *          ^
+ *          |
+ *          |
+ *          |___________________> Log10(Prime) avec un nombre premier choisit pour créer une échelle logarithmique.
+ */
+void drawPlotForPrimesFactorisation(){
+
+}
---
+++ benchmark.h
@@ -0,0 +1,8 @@
+#ifndef PROJETGPU_BENCHMARK_H
+#define PROJETGPU_BENCHMARK_H
+
+#include "utils/chronoCPU.hpp"
+#include "primeBreakerCPU.hpp"
+
+
+#endif
