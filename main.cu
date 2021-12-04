710cef784bf63b1aaac441965357c16f3f6cf010
Salhi Faisal <salhi4@fst-o-i-212-11.unilim.fr>
2021-12-04 11:16:27 +0100

    Ajouter la librairie pour utiliser gnu-plot au projet.\nNettoyer le fichier main.cu.\nModifier le .gitignore pour qu'il s'adapte aux salles de TPs.\n

--- main.cu
+++ main.cu
@@ -45,35 +45,6 @@ int main( int argc, char **argv )
 
 	uint64_t N=atoll(argv[1]);
 
-	/*cout << "============================================"	<< endl;
-	cout << "         Sequential version on CPU          " 	<< endl;
-	cout << "============================================"	<< endl << endl;
-	cout << " Partie CPU sur le nombre  " + to_string(N)<< endl;
-	ChronoCPU chrCPU;
-	chrCPU.start();
-	vector<uint64_t> tab_possibles_diviseurs(0);
-	bool isPrime = isPrimeCPU_v1(N,tab_possibles_diviseurs); //isPrimeCPU(N);
-	chrCPU.stop();
-	const float timeComputeCPUIsPrime = chrCPU.elapsedTime();
-	cout << "Temps du test de primalite : "	<< timeComputeCPUIsPrime << " ms" << endl;
-	cout << " Est Premier ? " << isPrime << endl;
-	cout << " Recherche des nombres premiers sur CPU " << endl;
-	chrCPU.start();
-	vector<uint64_t> primesNumbers = searchPrimesCPU_v0(N);
-	chrCPU.stop();
-	const float timeComputeCPUSearchPrime = chrCPU.elapsedTime();
-  //cout << printPrimes(primesNumbers) << endl; //afficher les nombres premiers //
-    cout << "Temps de recherche : "	<< timeComputeCPUSearchPrime << " ms" << endl;
-	cout << " Factorisation en nombre premier  sur CPU " << endl;
-	chrCPU.start();
-  vector<cell> facteurs(0);
-  factoCPU(N,&facteurs);
-	chrCPU.stop();
-	const float timeComputeCPUFact = chrCPU.elapsedTime();
-	cout << "Temps de factorisation en nombre premier : "	<< timeComputeCPUFact << " ms" << endl;
-	cout << " Factorisation CPU : " << printFactuers(facteurs)<<endl ; */
-
-
 	cout << "============================================"	<< endl;
 	cout << "          Parallel versions on GPU           "	<< endl;
 	cout << "============================================"	<< endl << endl;
@@ -90,8 +61,5 @@ int main( int argc, char **argv )
 	cell *facteursGPU;
 	float timeComputeGPUFact = launchKernelFactGPU<0>(N,primesNumbersGPU,facteursGPU,taille);
 	cout << "Temps de factorisation en nombre premier : "	<< timeComputeGPUFact << " ms" << endl;
-	//cout << " Factorisation GPU : "<< printFactuers(facteursGPU)<<endl;
-
-
 	return EXIT_SUCCESS;
 }
